e0975e41e32b4d4b0c1899dfdb4ad611aa9f7cf3
hpt-dan@163.com <17779609091@163.com>
2019-09-27 01:12:34 +0800

    创建第一个类

---
+++ src/main/java/com/wanbo/test/Test01.java
@@ -0,0 +1,18 @@
+package com.wanbo.test;
+
+/**
+ * @package: com.wanbo.test
+ * @version: V1.0
+ * @author: Administrator
+ * @date: 2019/4/21 22:12
+ * @description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("这是dev分支");
+        System.out.println("第一次修改");
+        System.out.println("这是master分支");
+        System.out.println("这是怎么了");
+        System.out.println("dev分支不会改变");
+    }
+}
